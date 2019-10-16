cebf7df56397f5e3c432fc8d3e4902e84ea54e76
Sarina Xie <sx2166@columbia.edu>
2019-10-16 17:05:11 -0400

    current files

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask import render_template
 from flask import Response, request, jsonify
+import os
+import subprocess
+
 app = Flask(__name__)
 
 subs = []
 cur_sub_id = 0
+img_fn = ""
+
 @app.route('/')
 def home():
     return render_template('home.html', title='home', subs=subs)
+
+@app.route('/textbox')
+def textbox():
+    return render_template('textbox.html', title='textbox', subs=subs)
 
 @app.route('/save_entry', methods=['GET','POST'])
 def save_entry():
@@ -33,17 +42,32 @@ def delete_entry():
             subs.remove(record)
     return jsonify(subs=subs)
 
+
 @app.route('/otp')
 def otp():
     return render_template('otp.html', title='otp')    
+
 
 @app.route('/canvas')
 def canvas():
     return render_template('canvas.html', title='canvas')
 
-@app.route('/test')
-def testcanvas():
-    return render_template('test.html', title='testcanvas')
+
+@app.route('/fe3h')
+def fe3h():
+    return render_template('fe3h.html', title='fe3h')
+
+@app.route('/send_time', methods=['GET', 'POST'])
+def send_time():
+    print("in send time)")
+    json_data = request.get_json()
+    timestamp = json_data['time']
+    os.chdir('/mnt/c/Users/Sarina/Documents/websites/me/static/images')
+    subprocess.call('rm byleth.png', shell=True)
+    subprocess.call('ffmpeg -ss 00:00:25 -i /mnt/c/Users/Sarina/Documents/websites/me/byleth_edelgard.mp4 -frames:v 1 byleth.png', shell=True)
+    return jsonify("ok")
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
