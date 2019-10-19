749f67db496bbfc2db523c9527022ba1b507027e
Sarina Xie <sx2166@columbia.edu>
2019-10-19 13:46:06 -0400

    canvas progress

--- static/layout.js
+++ static/layout.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     //doesn't work for dropdown navbar items
     //dropdown inactive items should get a "gray" class
     $('nav li').each(function(){
-        console.log(window.location.pathname, $(this).find('a'), $(this).find('a').attr('href'))
-        if(window.location.pathname.indexOf($(this).find('a').attr('href')) > -1) {
+        //console.log(window.location.pathname, $(this).find('a'), $(this).find('a').attr('href'))
+        if(window.location.pathname == ($(this).find('a').attr('href'))) {
             $(this).toggleClass('active')
-        }  
+        }
     })
 })
