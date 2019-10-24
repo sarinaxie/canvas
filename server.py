97ae989ad79c74fd60c5629019600c587e180ca6
Sarina Xie <sx2166@columbia.edu>
2019-10-24 19:34:51 -0400

    input change lol

--- server.py
+++ server.py
@@ -65,13 +65,14 @@ def send_time():
     json_data = request.get_json()
     timestamp = json_data['time']
 
-    os.chdir('/mnt/c/Users/Sarina/Documents/websites/me/static/images')
+    os.chdir('/mnt/c/Users/Sarina/Documents/websites/fe3h/static/images')
+    # os.chdir('/home/ec2-user/fe3h/static/images')
     if counter > 0:
         old_fname = str(counter-1) + '.png'
         rm_call = 'rm ' + old_fname
         subprocess.call(rm_call, shell=True)    
     fname = str(counter) + '.png'
-    ffmpeg_call = 'ffmpeg -hide_banner -ss ' + timestamp + ' -i /mnt/c/Users/Sarina/Documents/websites/me/byleth_edelgard.mp4 -vf scale=768:-1 -frames:v 1 ' + fname
+    ffmpeg_call = 'ffmpeg -hide_banner -ss ' + timestamp + ' -i /home/ec2-user/fe3h/byleth_edelgard.mp4 -vf scale=768:-1 -frames:v 1 ' + fname
     subprocess.call(ffmpeg_call, shell=True)
     counter += 1
     return jsonify(fname = fname)
